2fc480ef1c8516bfdfaaa84af56aba27148f0296
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2024-08-16 07:09:04 -0500

    feat: :sparkles: Add new routes for login, register, logout, and protected endpoints

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/login', (req, res) => {})
+
+app.post('/register', (req, res) => {})
+
+app.post('/logout', (req, res) => {})
+
+app.post('/protected', (req, res) => {})
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
 })
